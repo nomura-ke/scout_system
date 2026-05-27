ea178f05b8bdd19f82b090ff061e7418769b1658
nomura-ke <nomura-ke@lakeel.com>
2026-05-27 14:48:33 +0900

    変更

--- ScoutDX_template/ScoutDX_template/frontend/src/App.vue
+++ ScoutDX_template/ScoutDX_template/frontend/src/App.vue
@@ -1,15 +1,11 @@
 <template>
   <loginView />
-  <userRegisterView />
-  <roleTuggleView />
-  <scoutListView />
+  
   <RouterView />
 </template>
 
 <script setup>
 import loginView from './views/loginView.vue';
-import userRegisterView from './views/userRegisterView.vue';
-import roleTuggleView from './views/roleTuggleView.vue';
-import scoutListView from './views/scoutListView.vue';
+
 
 </script>
