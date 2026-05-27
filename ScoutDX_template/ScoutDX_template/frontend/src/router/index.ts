9dd07e9e1bc976b59342c52602fbb43a4d51df1b
takaoka-mi <takaoka-mi@lakeel.com>
2026-05-27 14:49:37 +0900

    スカウト文一覧追加

--- ScoutDX_template/ScoutDX_template/frontend/src/router/index.ts
+++ ScoutDX_template/ScoutDX_template/frontend/src/router/index.ts
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import ScoutPage from '../components/ScoutPage.vue'
-
+import ScoutMessageList from '../views/scoutListView.vue'
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'home',
     component: ScoutPage,
+  },
+  {
+    path: '/scout-messages',
+    name: 'scout-messages',
+    component: ScoutMessageList,
   },
 ]
 
